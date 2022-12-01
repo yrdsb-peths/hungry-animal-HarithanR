10dc5c9fbafcde63a15630234777fd683a0b969b
HarithanR <117937247+HarithanR@users.noreply.github.com>
2022-12-01 12:44:33 -0500

    Add files via upload

---
+++ hungry-animal-HarithanR-main-copy/Elephant.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The elephant, the player character.
+ * 
+ * @author Harithan Raveendran 
+ * @version December 2022
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(1);
+    }
+}
